0af0f39048455e10edd9d6d130c42cb1c7199fbb
Arif14377 <arifrahman14377@gmail.com>
2026-02-23 11:20:14 +0700

    feat: finish all query

--- README.md
+++ README.md
@@ -2,7 +2,14 @@
 
 - Mencari movie dengan tahun rilis lebih dari tahun 2000
 ![alt text](assets/img/movie-2000-up.png)
+
 - Mencari actors dengan akhiran nama 's'
+![alt text](assets/img/actors-with-s-lastname.png)
+
 - Mencari movie dengan rating diantara 5 dan 7 dan tahun rilis 2004 sampai 2006
+![alt text](assets/img/movies-rate57-20042006.png)
+
 - menghitung jumlah movie dengan rating 6
+![alt text](assets/img/movies-rank2.png)
+
 - Kumpulkan pada repo koda-b6-db3 dengan nama file query1.sql
--- query1.sql
+++ query1.sql
@@ -1,2 +1,19 @@
 SELECT "id", "name", "year", "rankscore"
-FROM movies where "year" > 2000 order by "id" desc limit 10 OFFSET 0;
+FROM movies where "year" > 2000 ORDER BY "id" DESC LIMIT 10 OFFSET 0;
+
+SELECT count("id")
+FROM movies where "year" > 2000;
+
+SELECT "id", "first_name", "last_name", "gender"
+FROM actors WHERE "last_name" ilike '%s'
+ORDER BY "id" ASC LIMIT 15 OFFSET 0;
+
+SELECT COUNT("first_name")
+FROM actors WHERE "first_name" ilike '%s';
+
+SELECT "id", "name", "year", "rankscore"
+FROM movies WHERE "rankscore" >= 5 AND "rankscore" <= 7 AND "year" >= 2004 AND "year" <= 2006
+ORDER BY "rankscore" DESC LIMIT 10 OFFSET 0;
+
+SELECT COUNT("rankscore")
+FROM movies WHERE "rankscore" = 6;
